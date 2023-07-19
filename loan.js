282b4648068352ea23fc9d6601bde01afc67d943
Sri <arpithatshankar17@gmail.com>
2023-07-19 14:37:13 +0530

    first commit

---
+++ loan.js
@@ -0,0 +1,15 @@
+function calculateLoan(){
+    var amount=document.getElementById('loanAmount').value;
+    var interest=document.getElementById('interestRate').value;
+    var tenure=document.getElementById('tenure').value;
+
+    var monthlyRate= interest / 100 / 12;
+    var monthlyPay = ( amount * monthlyRate) / ( 1 - Math.pow(1 + monthlyRate, -tenure));
+    var totalPay = monthlyPay * tenure;
+    var totalInterest = totalPay - amount;
+
+    document.getElementById('monthlyPay').innerHTML=`EMI : Rs` + monthlyPay.toFixed(2);
+    document.getElementById('totalPay').innerHTML=`Total Pay :` + totalPay.toFixed(2);
+    document.getElementById('totalInterest').innerHTML=`Total Interest` + totalInterest.toFixed(2);
+
+}
